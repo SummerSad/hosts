726c1c94d4ae69ded380cae5a79961c4c2a508d1
Tran Hau <hauvipapro@gmail.com>
2018-02-03 23:46:28 +0700

    remove tizbit cause it outdate

--- README.rst
+++ README.rst
@@ -59,9 +59,6 @@ Sources of hosts
 `win10 spy
 <https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win10/spy.txt>`_
 
-`tyzbit
-<https://raw.githubusercontent.com/tyzbit/hosts/master/data/tyzbit/hosts>`_
-
 `antipopads
 <https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts>`_
 
--- upd_src_hosts.ps1
+++ upd_src_hosts.ps1
@@ -15,7 +15,6 @@ $list = @("http://someonewhocares.org/hosts/zero/hosts",
         "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win7/spy.txt",
         "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win81/spy.txt",
         "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win10/spy.txt",
-        "https://raw.githubusercontent.com/tyzbit/hosts/master/data/tyzbit/hosts",
         "https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts",
         "https://raw.githubusercontent.com/yous/YousList/master/hosts.txt",
         "https://raw.githubusercontent.com/anudeepND/blacklist/master/CoinMiner.txt",
@@ -34,4 +33,4 @@ for($i = 0; $i -lt $list.length; ++$i){
 .\merge_host.exe raw .\hosts\5.txt .\hosts\6.txt .\hosts\7.txt .\hosts\8.txt .\hosts\9.txt
 .\merge_host.exe raw .\hosts\10.txt .\hosts\11.txt .\hosts\12.txt .\hosts\13.txt .\hosts\14.txt
 .\merge_host.exe raw .\hosts\15.txt .\hosts\16.txt .\hosts\17.txt .\hosts\18.txt .\hosts\19.txt
-.\merge_host.exe raw .\hosts\20.txt .\hosts\21.txt .\hosts\22.txt .\hosts\23.txt .\hosts\24.txt
+.\merge_host.exe raw .\hosts\20.txt .\hosts\21.txt .\hosts\22.txt .\hosts\23.txt
