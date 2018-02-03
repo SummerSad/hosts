39f200feebdd2a8091d83670fe364718c0411860
Tran Hau <hauvipapro@gmail.com>
2018-02-03 21:49:29 +0700

    add ::1 for ipv6

--- main.c
+++ main.c
@@ -204,7 +204,7 @@ void insert_need_file(const char *name)
 		return;
 	}
 
-	char *begin[] = {"127.0.0.1 localhost"};
+	char *begin[] = {"127.0.0.1 localhost", "::1 localhost"};
 	int size = sizeof(begin) / sizeof(begin[0]);
 	for (int i = 0; i < size; ++i)
 		fprintf(f_write, "%s\n", begin[i]);
